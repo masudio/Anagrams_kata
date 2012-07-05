87c32dbf5db974a594cebb02b41440eda4a96e3b
Masud Khan <masudio@gmail.com>
2012-07-05 11:07:04 -0600

    Initial commit.  Added BloomFilter project to build path to use wordlist.txt.

---
+++ src/Program.java
@@ -0,0 +1,8 @@
+import org.masudio.spellchecker.IAmADictionary;
+import org.masudio.spellchecker.WordList;
+
+
+public class Program
+{
+	IAmADictionary dictionary = new WordList("/lib/wordlist.txt");
+}
